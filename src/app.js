fc309762126944b8722fd5dfd1063cc5f96b15f5
DevSalma <devsalma6@gmail.com>
2023-05-09 12:14:39 +0100

    Added app.get th at returns an array containing objects

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
+app.get('/restaurants', async(request,response) => {
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
+});
 
 
 
